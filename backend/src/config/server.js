9197ac3fdffe45258b110cd00e283fb6c66b048a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-05 13:38:03 -0200

    Desenvolvimento do arquivo 'server.js'

---
+++ backend/src/config/server.js
@@ -0,0 +1,19 @@
+/**
+ * Data: 05/11/2017
+ * Arquivo: config/server.js
+ * Descrição: Arquivo responsável por levantar o serviço no lado do Back-End da aplicação.
+ * Author: Glaucia Lemos
+ */
+
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server  = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log('Back-End is running on port ' + port)
+})
---
+++ backend/src/loader.js
@@ -0,0 +1,6 @@
+/**
+ * Data: 05/11/2017
+ * Arquivo: src/loader.js
+ * Descrição: Arquivo responsável por realizar o carregamentos dos arquivos da pasta 'config'.
+ * Author: Glaucia Lemos
+ */
