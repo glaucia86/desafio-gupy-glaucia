02c53d0d03982ed67642fca7abbdc512f19527a9
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-05 21:17:57 -0200

    Testando o React...

---
+++ frontend/src/index.jsx
@@ -0,0 +1,12 @@
+/**
+ * Arquivo: src/index.jsx
+ * Data: 05/11/2017
+ * Descrição: Arquivo responsável pela interação do DOM com a página.
+ * Author: Glaucia Lemos
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,17 @@
+/**
+ * Arquivo: ./main/app.jsx
+ * Data: 05/11/2017
+ * Descrição: Arquivo responsável pelos Componentes da aplicação.
+ * Author: Glaucia Lemos
+ */
+
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+export default props => (
+    <div className="container">
+        <h1>Testando....</h1>
+    </div>
+)
